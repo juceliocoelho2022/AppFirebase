8516b5b23713d3546364d1ae3ab34c2dc0bd7689
JUCELIO <juceliocoelho2022@gmail.com>
2022-07-29 08:05:39 -0300

    Initial commit

---
+++ app/src/main/java/com/android/appfirebase/view/formcadastro/FormCadastro.kt
@@ -0,0 +1,12 @@
+package com.android.appfirebase.view.formcadastro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.android.appfirebase.R
+
+class FormCadastro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_cadastro)
+    }
+}
